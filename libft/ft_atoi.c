99509be108b439bfcc72319898100b742a0c55cb
Hartley, Ameen, Vodacom South Africa <Ameen.Hartley@vodacom.co.za>
2020-06-04 08:51:28 +0200

    ft_atoi updated

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -19,22 +19,19 @@ int	ft_atoi(char const *str)
 	size_t	i;
 
 	nbr = 0;
-	negative = 0;
+	negative = 1;
 	i = 0;
 	while (ft_iswhitespace(str[i]))
 		i++;
 	if (str[i] == '-')
-		negative = 1;
+		negative = -1;
 	if (str[i] == '+' || str[i] == '-')
 		i++;
 	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
 	{
 		nbr *= 10;
-		nbr += (int)str[i] - '0';
+		nbr += str[i] - '0';
 		i++;
 	}
-	if (negative == 1)
-		return (-nbr);
-	else
-		return (nbr);
+	return (nbr * negative);
 }
