3ca8802b57360582eab4cfc67f56353910da8fde
Hartley, Ameen, Vodacom South Africa <Ameen.Hartley@vodacom.co.za>
2020-06-04 09:26:32 +0200

    added xy coords

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -9,7 +9,8 @@ typedef struct		s_room
 {
 	int				room_id; //	id of room
 	int				ant_id; //	id of ant
-	int				tvd; //		counter for how many times transversed
+	int				run; //		counter for how many times transversed
+	int				xy[2]; //	x and y coordinates
 	char			*name; //	name of room
 	unsigned char	type; //	4 = end, 2 = start, 1 = occupied
 	struct s_room	**next; //	array of linked paths
