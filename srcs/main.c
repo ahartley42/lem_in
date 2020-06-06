6f3afb6a7650006379cd165c40578d8af7591816
tcoetzee-opus <tcoetzee.opus@gmail.com>
2020-06-06 03:59:37 -0700

    added .gitignroe

--- srcs/main.c
+++ srcs/main.c
@@ -1,11 +1,12 @@
-#include "includes/lem_in.h"
+#include "../includes/lem_in.h"
 
 static void initialize_struct(t_room *lem)//initialize 
 {
     lem->room_id = 0;
     lem->ant_id = 0;
     lem->run = 0;
-    lem->xy[2] = 0;
+    lem->xy[0] = 0;
+    lem->xy[1] = 0;
     lem->name = NULL;
     lem->type = 0;
 }
@@ -14,5 +15,6 @@ int main(void){
     t_room lem;
 
     initialize_struct(&lem);
+    write(1, "initialized", 11);
     return (0);
 }
