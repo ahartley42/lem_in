4f4dd3175d30b8915aa1e633264f9b6424e4f930
Deathshadowown <shanervanniekerk@gmail.com>
2020-06-08 22:32:39 +0200

    Upload for testing functions and how to use it

--- srcs/main.c
+++ srcs/main.c
@@ -14,165 +14,66 @@ static void initialize_struct(t_room *lem)//initialize
     lem->next = NULL;
 }
 
-int    ft_check_ants(char *base)
-{
-    int ant;
+// int    ft_check_ants(char *base)
+// {
+//     int ant;
 
-    ant = ft_atoi(base);
-    if(ant > 2147483647 || ant < 1){
-        ft_putstr("Error: bigger or smaller than int size \n");
-        exit(1);
-    }
-    ft_putstr("works \n");
-    return (ant);
-}
+//     ant = ft_atoi(base);
+//     if(ant > 2147483647 || ant < 1){
+//         ft_putstr("Error: bigger or smaller than int size \n");
+//         exit(1);
+//     }
+//     ft_putstr("works \n");
+//     return (ant);
+// }
 
 // checks to see if there is a start and an end
-int    ft_check_start_end(char *twodarray)
-{
-     if (ft_strequ(twodarray, "##start"))
-    {
-        return (0);
-    }
-    else if (ft_strequ(twodarray, "##end"))
-    {
-        return (1);
-    }else // will have to remove
-    {
-        ft_putstr("Error: no known command");
-        exit(1);
-    }
-}
+// int    ft_check_start_end(char *twodarray)
+// {
+//      if (ft_strequ(twodarray, "##start"))
+//     {
+//         return (0);
+//     }
+//     else if (ft_strequ(twodarray, "##end"))
+//     {
+//         return (1);
+//     }else // will have to remove
+//     {
+//         ft_putstr("Error: no known command");
+//         exit(1);
+//     }
+// }
 
-// keeping track if two starts or ends
-void ft_keep_track_start_end(int total,int *start,int *end)
-{
-    int st;
-    int ed;
-    st = *start; // keeping track how many start and ends it finds
-    ed = *end;
-    if (total == 0)
-    {
-        st++;
-        *start = st;
-        if (st == 2)
-        {
-            ft_putstr("Error: two starts error Program Exit\n");
-            exit(1);
-        }
-    }
-    if (total == 1)
-    {
-        ed++;
-        *end = ed;
-        if (ed == 2)
-        {
-            ft_putstr("Error: two ends error Program Exit\n");
-            exit(1);
-        }
-    }
-}
+// // keeping track if two starts or ends
+// void ft_keep_track_start_end(int total,int *start,int *end)
+// {
+//     int st;
+//     int ed;
+//     st = *start; // keeping track how many start and ends it finds
+//     ed = *end;
+//     if (total == 0)
+//     {
+//         st++;
+//         *start = st;
+//         if (st == 2)
+//         {
+//             ft_putstr("Error: two starts error Program Exit\n");
+//             exit(1);
+//         }
+//     }
+//     if (total == 1)
+//     {
+//         ed++;
+//         *end = ed;
+//         if (ed == 2)
+//         {
+//             ft_putstr("Error: two ends error Program Exit\n");
+//             exit(1);
+//         }
+//     }
+// }
 
-// looking for space in ft_strlen if it comes accross L or - in a name it will error out
-int ft_strlen_space(char *str)
-{
-    size_t i;
-    
-    i = 0;
 
-    if(str[i] == 'L')
-    {
-        ft_putstr("Error: cannot have caps L at the start of the name \n");
-        exit(1);
-    }
-    while(str[i] != '\0')
-    {
-        // if (str[i] == '-')
-        // {
-        //     ft_putstr("Error: cannot have a - in the name");
-        //     exit(1);
-        // }
-        if (str[i] == ' ')
-        {
-            break;
-        }
-        i++;
-    }
-    return (i);
-}
-
-int ft_strlen_space_error(char *str)
-{
-    size_t i;
-    
-    i = 0;
-
-    if(str[i] == 'L')
-    {
-        ft_putstr("Error: cannot have caps L at the start of the name \n");
-        exit(1);
-    }
-    while(str[i] != '\0')
-    {
-        if (str[i] == '-')
-        {
-            ft_putstr("Error: cannot have a - in the name \n");
-            exit(1);
-        }
-        if (str[i] == ' ')
-        {
-            break;
-        }
-        i++;
-    }
-    return (i);
-}
-
-void ft_strlen_space_count(char *str)
-{
-    int i;
-    int k;
-
-    i = 0;
-    k = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] == ' ')
-        k++;
-        i++; 
-    }
-    if (k > 2 || k < 2)
-    {
-        ft_putstr("Error: Badly Formatted Map, room invalid \n");
-        exit(1);
-    }
-}
-
-int    ft_strlen_pipes(char *str)
-{
-    int i;
-    int j;
-    
-    i = 0;
-    j = 0;
-    if(str[i] == 'L')
-    {
-        ft_putstr("Error: cannot have caps L at the start of the name \n");
-        exit(1);
-    }
-    while (str[i] != '\0')
-    {
-        if (str[i] == '-')
-        j++;
-        i++;
-    }
-    printf("j: %d \n", j);
-    if (j != 1){
-        ft_putstr("Error: Badly Formatted Map, pipe invalid \n");
-        exit(1);
-    }
-    return (1);
-}
 
 // int ft_check_ants_and_skip_comments_on_top(char **twodarray, *ant_amount)
 // {
@@ -193,102 +94,102 @@ int    ft_strlen_pipes(char *str)
 //     return (i);
 // }
 
-void check_file(t_room *lem, char **twodarray, int *ant_amount)
-{
-    int i;
-    int start;  // will be removed for struct
-    int end;    // will be removed for struct
-    int total;
-    int strlen;
-    int strlenpipe;
+// void verify_map_and_data(t_room *lem, char **twodarray, int *ant_amount)
+// {
+//     int i;
+//     int start;  // will be removed for struct
+//     int end;    // will be removed for struct
+//     int total;
+//     int strlen;
+//     int strlenpipe;
 
-    lem->room_id = 1; // remove because lem not been used yet
-    i = 0;
-    start = 0;
-    end = 0;
-    total = 0;
-    strlen = 0;
-    strlenpipe = 0;
-    // i = ft_check_ants_and_skip_comments_on_top(**twodarray, &ant_amount); // not working right now
+//     lem->room_id = 1; // remove because lem not been used yet
+//     i = 0;
+//     start = 0;
+//     end = 0;
+//     total = 0;
+//     strlen = 0;
+//     strlenpipe = 0;
+//     // i = ft_check_ants_and_skip_comments_on_top(**twodarray, &ant_amount); // not working right now
 
-    // looking for ant_amount once found breaks out and returns ant amount  if not found returns error
-    while (twodarray[i])
-    {
-        if (twodarray[i][0] == '#') {
-            if (ft_strequ(twodarray[i], "##start") || ft_strequ(twodarray[i], "##end")){
+//     // looking for ant_amount once found breaks out and returns ant amount  if not found returns error
+//     while (twodarray[i])
+//     {
+//         if (twodarray[i][0] == '#') {
+//             if (ft_strequ(twodarray[i], "##start") || ft_strequ(twodarray[i], "##end")){
 
-            }
-            else{
-                i++;
-            }
-        }
-        if (check_int(twodarray[i]) == 1)
-        {
-            *ant_amount = ft_check_ants(twodarray[i]);
-            break;
-        }
-        i++;
-    }
-    if (*ant_amount == 0)
-    {
-        ft_putstr("Error: please enter ant's\n");
-        exit(1);
-    }
-    i++; // incrementing past the ant_amount
-    while (twodarray[i])
-    {
-        if (twodarray[i][0] == '#' && !(ft_strequ(twodarray[i], "##start") || ft_strequ(twodarray[i], "##end"))){
-            printf("skiped: %s\n",twodarray[i]);
-        i++;
-        }
-        if (check_int(twodarray[i]) == 1)
-        {
-            ft_putstr("Error: Badly Formatted Map \n");
-            exit(1);
-        }
-        if (twodarray[i][0] == '#' && twodarray[i][1] == '#')
-        {
-            total = ft_check_start_end(twodarray[i]);
-            ft_keep_track_start_end(total, &start, &end);
-            // ft_add_Command_rooms(twodarray[i + 1], total);
-        }
-        if ((strlen = ft_strlen_space(twodarray[i])) && twodarray[i][strlen] == ' ' && twodarray[i][strlen + 1]){
-            // printf("was here\n");
-            char    **check_rooms;
-            ft_strlen_space_count(twodarray[i]); // counting the amount of spaces more or less than 2 displays error
-            check_rooms = ft_strsplit(twodarray[i], ' ');
-            ft_strlen_space_error(check_rooms[0]); // checks for name starting with L and - inside the name
-            if (check_int(check_rooms[1]) == 1 && check_int(check_rooms[2]) == 1)
-            {
-                printf("Correct \n");
-            }else
-            {
-                ft_putstr("Error: Badly Formatted Map X Y not integer \n");
-                exit(1);
-            }
-            printf("room check: %s\n", check_rooms[0]);
-            free2dArray(check_rooms);
-        }
-        if ((strlenpipe = ft_strlen_space(twodarray[i])) && twodarray[i][strlenpipe] == '\0' && twodarray[i][0] != '#')
-        {
-            // printf("%s \n", twodarray[i]);
-            // printf("we got inside here \n");
-            strlenpipe = 0;
-            if (( strlenpipe = ft_strlen_pipes(twodarray[i])) == 1)
-            {
-                printf("Correct pipe \n");
-            }
-            else{
-                printf("Error: Invalid Pipe \n");
-            }
+//             }
+//             else{
+//                 i++;
+//             }
+//         }
+//         if (check_int(twodarray[i]) == 1)
+//         {
+//             *ant_amount = ft_check_ants(twodarray[i]);
+//             break;
+//         }
+//         i++;
+//     }
+//     if (*ant_amount == 0)
+//     {
+//         ft_putstr("Error: please enter ant's\n");
+//         exit(1);
+//     }
+//     i++; // incrementing past the ant_amount
+//     while (twodarray[i])
+//     {
+//         if (twodarray[i][0] == '#' && !(ft_strequ(twodarray[i], "##start") || ft_strequ(twodarray[i], "##end"))){
+//             printf("skiped: %s\n",twodarray[i]);
+//         i++;
+//         }
+//         if (check_int(twodarray[i]) == 1)
+//         {
+//             ft_putstr("Error: Badly Formatted Map \n");
+//             exit(1);
+//         }
+//         if (twodarray[i][0] == '#' && twodarray[i][1] == '#')
+//         {
+//             total = ft_check_start_end(twodarray[i]);
+//             ft_keep_track_start_end(total, &start, &end);
+//             // ft_add_Command_rooms(twodarray[i + 1], total);
+//         }
+//         if ((strlen = ft_strlen_space(twodarray[i])) && twodarray[i][strlen] == ' ' && twodarray[i][strlen + 1]){
+//             // printf("was here\n");
+//             char    **check_rooms;
+//             ft_strlen_space_count(twodarray[i]); // counting the amount of spaces more or less than 2 displays error
+//             check_rooms = ft_strsplit(twodarray[i], ' ');
+//             ft_strlen_space_error(check_rooms[0]); // checks for name starting with L and - inside the name
+//             if (check_int(check_rooms[1]) == 1 && check_int(check_rooms[2]) == 1)
+//             {
+//                 printf("Correct \n");
+//             }else
+//             {
+//                 ft_putstr("Error: Badly Formatted Map X Y not integer \n");
+//                 exit(1);
+//             }
+//             printf("room check: %s\n", check_rooms[0]);
+//             free2dArray(check_rooms);
+//         }
+//         if ((strlenpipe = ft_strlen_space(twodarray[i])) && twodarray[i][strlenpipe] == '\0' && twodarray[i][0] != '#')
+//         {
+//             // printf("%s \n", twodarray[i]);
+//             // printf("we got inside here \n");
+//             strlenpipe = 0;
+//             if (( strlenpipe = ft_strlen_pipes(twodarray[i])) == 1)
+//             {
+//                 printf("Correct pipe \n");
+//             }
+//             else{
+//                 printf("Error: Invalid Pipe \n");
+//             }
             
-        }
-        i++;
-        // printf("i: %d\n", i);
-        // printf("strlen: %d\n", strlen);
-        strlen = 0;
-    }
-}
+//         }
+//         i++;
+//         // printf("i: %d\n", i);
+//         // printf("strlen: %d\n", strlen);
+//         strlen = 0;
+//     }
+// }
 
 int main(void){
     t_room lem;
@@ -313,7 +214,7 @@ int main(void){
     // printf("%s\n", &array[1][4]);
 
     //useing
-    check_file(&lem, array, &ant_amount);
+    verify_map_and_data(&lem, array, &ant_amount);
     printf("%d", ant_amount);
     free2dArray(array);
 
