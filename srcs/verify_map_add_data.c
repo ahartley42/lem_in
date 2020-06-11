538d8e7156badf0a6188c42d5a189379f3b577f7
Deathshadowown <shanervanniekerk@gmail.com>
2020-06-11 13:33:59 +0200

    updated

--- srcs/verify_map_add_data.c
+++ srcs/verify_map_add_data.c
@@ -60,16 +60,19 @@ void ft_keep_track_start_end(int total,int *start,int *end)
 
 void verify_map_and_data(t_room *lem, char **twodarray, int *ant_amount)
 {
-    int i;
-    int start;  // will be removed for struct
-    int end;    // will be removed for struct
-    int total;
-    int strlen;
-    int strlenpipe;
-    int pipeTrack;
-    int roomTrack;
+    // char    nameTrack[] = "";
+    int     i;
+    int     start;  // will be removed for struct
+    int     end;    // will be removed for struct
+    int     total;
+    int     strlen;
+    int     strlenpipe;
+    int     pipeTrack;
+    int     roomTrack;
 
     lem->room_id = 1; // remove because lem not been used yet
+    // nameTrack = "wee";
+    // printf("%s", nameTrack);
     i = 0;
     start = 0;
     end = 0;
@@ -116,28 +119,30 @@ void verify_map_and_data(t_room *lem, char **twodarray, int *ant_amount)
     i++; // incrementing past the ant_amount
     while (twodarray[i])
     {
-        if (twodarray[i][0] == '#' && twodarray[i][1] != '#'){
+        if (twodarray[i][0] == '#' && twodarray[i][1] != '#'){ // checks for comment
         i++;
         }
-        if (check_int(twodarray[i]) == 1)
+        if (check_int(twodarray[i]) == 1) // checks to see if more than one ant
         {
             ft_putstr("Error: Badly Formatted Map \n");
             exit(1);
         }
-        if (twodarray[i][0] == '#' && twodarray[i][1] == '#')
+        if (twodarray[i][0] == '#' && twodarray[i][1] == '#') // checks for start and end
         {
             total = ft_check_start_end(twodarray[i]);
             ft_keep_track_start_end(total, &start, &end);
             // ft_add_Command_rooms(twodarray[i + 1], total);
         }
-        if ((strlen = ft_strlen_space(twodarray[i])) && twodarray[i][strlen] == ' ' && twodarray[i][strlen + 1]){
+        if ((strlen = ft_strlen_space(twodarray[i])) && twodarray[i][strlen] == ' ' && twodarray[i][strlen + 1]){ // checks for room valid
             char    **check_rooms;
             ft_strlen_space_count(twodarray[i]); // counting the amount of spaces more or less than 2 displays error
             check_rooms = ft_strsplit(twodarray[i], ' ');
             ft_strlen_space_error(check_rooms[0]); // checks for name starting with L and - inside the name
+            printf("what inside: %s", check_rooms[0]);
             if (check_int(check_rooms[1]) == 1 && check_int(check_rooms[2]) == 1)
             {
                 ft_putstr("Correct room \n");
+                printf("whats inside: %s \n", check_rooms[0]);
             }else
             {
                 ft_putstr("Error: Badly Formatted Map X Y not integer \n");
@@ -146,7 +151,7 @@ void verify_map_and_data(t_room *lem, char **twodarray, int *ant_amount)
             roomTrack++;
             free2dArray(check_rooms);
         }
-        if ((strlenpipe = ft_strlen_space(twodarray[i])) && twodarray[i][strlenpipe] == '\0' && twodarray[i][0] != '#')
+        if ((strlenpipe = ft_strlen_space(twodarray[i])) && twodarray[i][strlenpipe] == '\0' && twodarray[i][0] != '#') // checks to see if pipes is valid
         {
             strlenpipe = 0;
             printf("pipe: %s \n",twodarray[i]);
