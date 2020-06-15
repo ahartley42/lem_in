dce715c235bc7246846b60277c1f5fb0f78484d6
Deathshadowown <shanervanniekerk@gmail.com>
2020-06-15 18:35:45 +0200

    added error checking to see if start end if room exist and pipes + check to see if no fake name is inside pipe

--- srcs/error.c
+++ srcs/error.c
@@ -8,3 +8,53 @@ void	error(unsigned char e)
 		exit(1);
 	}
 }
+
+// checks to see if start and end, and checks to see if it has enterd pipes and rooms
+void ft_check_for_error(int *start, int *end, int *pipeTrack, int *roomTrack)
+{
+        if (*start < 1 || *end < 1)
+    {
+        ft_putstr("Error: Badly Formatted Map no start or end \n");
+        exit(1);
+    }
+    if (*pipeTrack < 1)
+    {
+        ft_putstr("Error: Badly Formatted no Pipes \n");
+        exit(1);
+    }
+    if (*roomTrack < 1)
+    {
+        ft_putstr("Error: Badly Formatted no rooms \n");
+        exit(1);
+    }
+}
+
+// checking to see if the name passed in pipes exist and is not fake
+void ft_wrong_name_error(char *firstName, char *lastName, t_room *lem_head)
+{
+    t_room *tmp;
+    int     count;
+    int     count2;
+
+    tmp = lem_head;
+    count = 0;
+    count2 = 0;
+    while ((tmp) != NULL)
+    {
+        if (ft_strequ(firstName, (tmp)->name))
+        count++;
+        if (ft_strequ(lastName, (tmp)->name))
+        count2++;
+        tmp = tmp->next;
+    }
+    if (count < 1)
+    {
+        ft_putendl("ERROR: name does not exist");
+        exit(1);
+    }
+    if (count2 < 1)
+    {
+        ft_putendl("ERROR: name does not exist");
+        exit(1);
+    }
+}
