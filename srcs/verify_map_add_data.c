f4a5faa4664e9698ab114d3642bc88af8424ac64
Deathshadowown <shanervanniekerk@gmail.com>
2020-06-15 18:39:59 +0200

    updated verify_map_add_data needs cleaning

--- srcs/verify_map_add_data.c
+++ srcs/verify_map_add_data.c
@@ -60,109 +60,6 @@ void ft_keep_track_start_end(int total,int *start,int *end,int *isStart,int *isE
     }
 }
 
-// void    ft_add_rooms(t_room *lem, char **room_data)
-// {
-//     int i;
-
-//     i = 0;
-//     lem->room_id = i;
-//     i++;
-//     lem->name = ft_strdup(room_data[0]);
-//     lem->xy[0] = ft_atoi(room_data[1]);
-//     lem->xy[1] = ft_atoi(room_data[2]);
-//     lem->ant_id = 0;
-//     lem->run = 0;
-//     lem->type = 0;
-//     lem->path = NULL;
-//     lem->next = NULL;
-//     lem->next = initialize_struct();
-// }
-
-void ft_check_for_error(int *start, int *end, int *pipeTrack, int *roomTrack)
-{
-        if (*start < 1 || *end < 1)
-    {
-        ft_putstr("Error: Badly Formatted Map no start or end \n");
-        exit(1);
-    }
-    if (*pipeTrack < 1)
-    {
-        ft_putstr("Error: Badly Formatted no Pipes \n");
-        exit(1);
-    }
-    if (*roomTrack < 1)
-    {
-        ft_putstr("Error: Badly Formatted no rooms \n");
-        exit(1);
-    }
-}
-
-int ft_strlen_hyphen(char **twodarray,int *i)
-{
-    int len;
-    int j;
-    j = *i;
-    len = 0;
-
-    while (twodarray[j][len] != '-')
-    {
-        len++;
-    }
-    return (len);
-}
-
-char	*ft_strcpy_to_hyphen(char *dest, const char *src, char n)
-{
-	int count;
-
-	count = 0;
-	while (src[count] != '\0' && src[count] != n)
-	{
-		dest[count] = src[count];
-		count++;
-	}
-    dest[count] = '\0';
-	return (dest);
-}
-
-char	*ft_strcpy_after_hyphen(char *dest, const char *src, char n)
-{
-	int count;
-    int count2;
-
-	count = 0;
-    count2 = 0;
-    while (src[count] != n)
-    count++;
-    count++;
-	while (src[count] != '\0')
-	{
-		dest[count2] = src[count];
-		count++;
-        count2++;
-	}
-    dest[count2] = '\0';
-	return (dest);
-}
-
-int    ft_strlen_till_end(char **twodarray, int *i, int *len)
-{
-    int j;
-    int length;
-    int len2;
-
-    j = *i;
-    length = *len;
-    len2 = 0;
-    length++;
-    while (twodarray[j][length] != '\0')
-    {
-        len2++;
-        length++;
-    }
-    return (len2);
-}
-
 char    *ft_find_firstName(char **twodarray, int *i)
 {
     char    *firstName;
@@ -176,6 +73,7 @@ char    *ft_find_firstName(char **twodarray, int *i)
     ft_strcpy_to_hyphen(firstName, twodarray[j], '-');
     return (firstName);
 }
+
 char    *ft_find_lastName(char **twodarray, int *i)
 {
     char    *lastName;
@@ -190,167 +88,6 @@ char    *ft_find_lastName(char **twodarray, int *i)
     lastName = ft_strnew(len2);
     ft_strcpy_after_hyphen(lastName, twodarray[j], '-');
     return (lastName);
-}
-
-// void    ft_malloc_path(t_room tmp,char *firstName,char *lastName)
-// {
-//     t_room *temp;
-//     int count;
-//     int count2;
-//     // int lastCount;
-
-//     temp = &tmp;
-//     count = 0;
-//     count2 = 0;
-//     // lastCount = 0;
-//     // printf("here \n");
-//     // printf("%s \n", firstName);
-//     // printf("%s \n", lastName);
-//     // while((temp) != NULL)
-// 	// {
-//     //     printf("room_id: %d \n", (temp)->room_id);
-// 	// 	printf("name: %s \n", (temp)->name);
-//     //     temp = temp->next;
-// 	// }
-
-
-//     // while ((temp) != NULL)
-//     // {
-//     //     if (ft_strequ(firstName, (temp)->name))
-//     //     {
-//     //         // printf("struct: %s \n", (temp)->name);
-//     //         // printf("firstName_inside: %s \n", firstName);
-//     //         count++;
-//     //         // printf("count: %d \n", count);
-//     //     }
-//     //     if (ft_strequ(lastName, (temp)->name))
-//     //     {
-//     //         // printf("struct: %s \n", (temp)->name);
-//     //         // printf("lastName_inside: %s \n", lastName);
-//     //         count2++;
-//     //         // printf("count: %d \n", count);
-//     //     }
-//     //     if (ft_strequ(firstName, (temp)->name) && ft_strequ(lastName, (temp)->name))
-//     //     {
-//     //         ft_putendl("ERROR");
-//     //         exit(1);
-//     //     }
-//     //     printf("struct: %s \n", (temp)->name);
-//     //     temp = temp->next;
-//     // }
-//     // if (count < 1 || count2 < 1)
-//     // {
-//     //     ft_putendl("ERROR: name of pipe does not exist");
-//     //     exit(1);
-//     // }
-//     // printf("count: %d \n", count);
-//     // printf("count: %d \n", count2);
-// }
-
-// void    ft_strlen_name_hold(t_room lem_head ,char **twodarray , int *i)
-// {
-//     t_room *tmp;
-//     char    *firstName;
-//     char    *lastName;
-//     char    *first2nd;
-//     char    *last2nd;
-//     int     strlenpipe;
-//     // int     len;
-//     // int     len2;
-//     int     j;
-//     int     k;
-
-//     tmp = &lem_head;
-//     j = *i;
-//     k = *i;
-//     strlenpipe = 0;
-//     // len = 0;
-//     // len2 = 0;
-//     // len = ft_strlen_hyphen(twodarray, i);
-//     // firstName = ft_strnew(len);
-//     // ft_strcpy_to_hyphen(firstName, twodarray[j], '-');
-//     // printf("firstName:%s \n", firstName);
-//     ///////////////////////////////////////////////////////////////////
-//     // len2 = ft_strlen_till_end(twodarray, i, &len);
-//     // lastName = ft_strnew(len2);
-//     // ft_strcpy_after_hyphen(lastName, twodarray[j], '-');
-//     // printf("lastName:%s\n", lastName);
-//     ////////////////////////////////////////////////////////////////////
-//     int count = 0;
-//     int count2 = 0;
-//     while (twodarray[j])
-//     {
-//         firstName = ft_find_firstName(twodarray, &j);
-//         lastName = ft_find_lastName(twodarray, &j);
-//     // will be using this
-//         if ((strlenpipe = ft_strlen_pipes(twodarray[j])) == 1)
-//         {
-//             // ft_malloc_path(*tmp, firstName, lastName);
-//             printf("array: %s \n",twodarray[j]);
-//             // tmp = &lem_head;
-//             while (twodarray[k])
-//             {
-//                 first2nd = ft_find_firstName(twodarray, &k);
-//                 last2nd = ft_find_lastName(twodarray, &k);
-//                 if (firstName == first2nd)
-//                 count++;
-//                 if (lastName == last2nd)
-//                 count2++;
-//                 free_two_strings(first2nd, last2nd);
-//             }
-//             k = *i;
-//             while (twodarray[k])
-//             {
-//                 if (firstName == last2nd)
-//                 count++;
-//                 if (lastName == firstName)
-//                 count++;
-//             }
-//             k = *i;
-//         }
-//         printf("next round \n");
-//         j++;
-//         free_two_strings(firstName, lastName);
-//     }
-//         // tmp = lem;
-//         // going to be useing this
-//     // 	while((tmp) != NULL)
-// 	// {
-//     //     printf("room_id: %d \n", (tmp)->room_id);
-// 	// 	printf("name: %s \n", (tmp)->name);
-//     //     tmp = tmp->next;
-// 	// }
-//     // free(firstName);
-//     // free(lastName);
-// }
-
-void ft_wrong_name(char *firstName, char *lastName, t_room *lem_head)
-{
-    t_room *tmp;
-    int     count;
-    int     count2;
-
-    tmp = lem_head;
-    count = 0;
-    count2 = 0;
-    while ((tmp) != NULL)
-    {
-        if (ft_strequ(firstName, (tmp)->name))
-        count++;
-        if (ft_strequ(lastName, (tmp)->name))
-        count2++;
-        tmp = tmp->next;
-    }
-    if (count < 1)
-    {
-        ft_putendl("ERROR: name does not exist");
-        exit(1);
-    }
-    if (count2 < 1)
-    {
-        ft_putendl("ERROR: name does not exist");
-        exit(1);
-    }
 }
 
 void    ft_add_pipe_address(t_room *lem_head,char **twodarray, int *i)
@@ -370,7 +107,7 @@ void    ft_add_pipe_address(t_room *lem_head,char **twodarray, int *i)
     {
         firstName = ft_find_firstName(twodarray, &j);
         lastName = ft_find_lastName(twodarray, &j);
-        ft_wrong_name(firstName, lastName, lem_head);
+        ft_wrong_name_error(firstName, lastName, lem_head);
         while ((tmp) != NULL)
         {
             if (ft_strequ(firstName, (tmp)->name))
@@ -407,11 +144,6 @@ void verify_map_and_data(t_room *lem_tmp, t_room *lem_head, char **twodarray, in
     int     isStart;
     int     isEnd;
 
-    // lem->room_id = 1; // remove because lem not been used yet
-    // lem->next = initialize_struct();
-    // lem->next->room_id = 2;
-    // nameTrack = "wee";
-    // printf("%s", nameTrack);
     i = 0;
     j = 0;
     start = 0;
