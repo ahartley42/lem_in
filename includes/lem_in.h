0443f0dd3afa91294a2b66f2e062ffc5d9608b35
Deathshadowown <shanervanniekerk@gmail.com>
2020-06-15 18:19:10 +0200

    updated lem_in.h

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -39,12 +39,7 @@ void				free_two_strings(char *s1, char *s2);
 char				**read_map();
 
 //verify_map
-<<<<<<< HEAD
-void				verify_map_and_data(t_room *lem, char **twodarray, int *ant_amount);
-=======
 void verify_map_and_data(t_room *lem_tmp, t_room *lem_head, char **twodarray, int *ant_amount);
->>>>>>> shane
-
 //strlen_modified
 int    				ft_strlen_pipes(char *str);
 void 				ft_strlen_space_count(char *str);
