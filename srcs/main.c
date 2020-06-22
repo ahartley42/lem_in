abfcffcc4de430e032d6c31ec8801cbfa9bf29dc
Hartley, Ameen, Vodacom South Africa <Ameen.Hartley@vodacom.co.za>
2020-06-22 12:01:36 +0200

    added path print to main

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,24 @@ int main(void){
     t_room *tmp;
     char    **map;
     int ant_amount;
+	int		i;
 
     lem = initialize_struct();
     ant_amount = 0;
+	i = 1;
     tmp = lem;
     map = read_map();
     ft_strlen_2d_array(map);
     verify_map_and_data(tmp, lem, map, &ant_amount);
+	tmp = lem;
+	while (!(tmp->type & 2))
+		tmp = tmp->next;
+	pathfinder(&tmp);
+	while (i <= ant_amount + path_size(&tmp))
+	{
+		print_solution(&tmp, ant_amount, i);
+		i++;
+	}
     // ft_putendl_free_exit("ERROR", map, lem);
     printf("%d", ant_amount);
     free2dArray(map);
