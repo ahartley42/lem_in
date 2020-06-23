d0fd97455ec51382cf10541a003872db5d804848
Deathshadowown <shanervanniekerk@gmail.com>
2020-06-23 19:23:48 +0200

    shorten function again should be fine now

--- srcs/verify_map_add_data.c
+++ srcs/verify_map_add_data.c
@@ -181,6 +181,25 @@ void    ft_check_for_random_ants(char **twodarray, int *i)
     *i = j;
 }
 
+void    ft_check_start_end_room(char **twodarray,int *ij, int total, unsigned char *dip_Switch)
+{
+    if (twodarray[ij[0]][0] == '#' && twodarray[ij[0]][1] == '#') // checks for start and end
+    {
+        total = ft_check_start_end(twodarray[ij[0]]);
+        ft_keep_track_start_end(total, dip_Switch);
+        if ((*dip_Switch & 32) == 32)
+        ft_check_start_room(twodarray, &ij[0]);
+        if ((*dip_Switch & 64) == 64)
+        ft_check_end_room(twodarray, &ij[0]);
+    }
+}
+
+void    ft_skip_comment(char **twodarray,int *ij)
+{
+        if (twodarray[ij[0]][0] == '#' && twodarray[ij[0]][1] != '#') // checks for comment
+        ij[0]++;
+}
+
 void    verify_map_and_data(t_room *lem_tmp, t_room *lem_head, char **twodarray, int *ant_amount)
 {
     int     ij[2];
@@ -197,18 +216,9 @@ void    verify_map_and_data(t_room *lem_tmp, t_room *lem_head, char **twodarray,
     ft_check_for_ant_amount(twodarray, &ij[0], ant_amount);
     while (twodarray[ij[0]])
     {
-        if (twodarray[ij[0]][0] == '#' && twodarray[ij[0]][1] != '#') // checks for comment
-        ij[0]++;
+        ft_skip_comment(twodarray, &ij[0]);
         ft_check_for_random_ants(twodarray, &ij[0]); // checks to see if more than one ant
-        if (twodarray[ij[0]][0] == '#' && twodarray[ij[0]][1] == '#') // checks for start and end
-        {
-            total = ft_check_start_end(twodarray[ij[0]]);
-            ft_keep_track_start_end(total, &dip_Switch);
-            if ((dip_Switch & 32) == 32)
-            ft_check_start_room(twodarray, &ij[0]);
-            if ((dip_Switch & 64) == 64)
-            ft_check_end_room(twodarray, &ij[0]);
-        }
+        ft_check_start_end_room(twodarray, ij, total, &dip_Switch);
         if ((strlen = ft_strlen_space(twodarray[ij[0]])) && twodarray[ij[0]][strlen] == ' ' && twodarray[ij[0]][strlen + 1]){ // checks for room valid
             ft_check_valid_room(lem_tmp, twodarray, ij, &dip_Switch);
             lem_tmp = lem_tmp->next;
